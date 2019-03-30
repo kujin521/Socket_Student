a090ee6545126613ea1d157a78b5ddc396ec1b21
kujin <kujinemil@foxmail.com>
2019-03-30 15:03:15 +0800

    初始提交（无代码）

---
+++ SocketClient/src/com/kj/socketclient/SocketClient.java
@@ -0,0 +1,13 @@
+package com.kj.socketclient;
+
+/**
+ * Author:  kujin
+ * Email:  1604323361@qq.com
+ * Date:   2019/3/30
+ * 类的意义：这是客户端
+ */
+public class SocketClient {
+    public static void main(String[] args) {
+
+    }
+}
